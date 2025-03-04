734277bdeea647809adaf854679197fca9f338da
Ayoola Olumide Olufemi <ayoolaolumide98@gmail.com>
2025-03-04 16:40:38 +0100

    responsive-fix

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -2,28 +2,28 @@ const data = [
     {
       "category": "Reaction",
       "score": 80,
-      "icon": "src/assets/images/icon-reaction.svg",
+      "icon": "./src/assets/images/icon-reaction.svg",
       "text-color": "text-Light_red",
       "bg-color": "bg-Light_red_bg"
     },
     {
       "category": "Memory",
       "score": 92,
-      "icon": "src/assets/images/icon-memory.svg",
+      "icon": "./src/assets/images/icon-memory.svg",
       "text-color": "text-rangey_yellow",
       "bg-color": "bg-rangey_yellow_bg"
     },
     {
       "category": "Verbal",
       "score": 61,
-      "icon": "src/assets/images/icon-verbal.svg",
+      "icon": "./src/assets/images/icon-verbal.svg",
       "text-color": "text-Green_teal",
       "bg-color": "bg-Green_teal_bg"
     },
     {
       "category": "Visual",
       "score": 72,
-      "icon": "src/assets/images/icon-visual.svg",
+      "icon": "./src/assets/images/icon-visual.svg",
       "text-color": "text-Cobalt_blue",
       "bg-color": "bg-Cobalt_blue_bg"
     }
